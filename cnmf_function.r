0c6fe97dc0ef11f22bda11aa2998939ef98837e8
Avishai Wizel <Avishai987@gmail.com>
2022-12-12 12:51:54 +0200

    improve program_assignment
    correct and implement clearer method

--- cnmf_function.r
+++ cnmf_function.r
@@ -477,34 +477,22 @@ sum_df_cols_to_1 <- function(df) {
 
 #assign program to each cell after add_prgorams_score
 #' @param larger_by how much a program should be greater then the second one to be assign as this program of this cell.
-program_assignment <- function(dataset,num_of_programs,larger_by = 1) {
-  programs_lst = c()
-  for (i in 1:num_of_programs) {
-    programs_lst = c(programs_lst,"metaProgram." %>% paste0(i))
-  }
-  scores_metadata = FetchData(object = dataset,vars = c(programs_lst))
+program_assignment <- function(dataset,larger_by = 1,program_names) {
+
+  scores_metadata = FetchData(object = dataset,vars = c(program_names))
   assignment_df =  data.frame(row.names = rownames(scores_metadata))
   
-  for (program_name in programs_lst) {
-    logical_df= data.frame(row.names = rownames(scores_metadata))
-    for (other_program_name in programs_lst) {
-      if(other_program_name == program_name){next}
-      val = (scores_metadata[program_name] > larger_by * scores_metadata[other_program_name])
-      logical_df = cbind(logical_df,val)
-      assignment_df[program_name] = rowSums(logical_df) == num_of_programs-1
-    }
+  find_max_program <- function(x,larger_by) { #find max program index, else: "NA"
+    n <- length(x)
+    max = sort(x)[n] #get max value
+    second_max = sort(x)[n-1] #get 2nd max value
+    if (max > larger_by*second_max){which.max(x)} else {"NA"} #if max is larger than 2nd max*larger_by, write max index. else, write "NA"
   }
+  assignment_df["assigment"] = apply(X = scores_metadata,MARGIN = 1,FUN = find_max_program,larger_by = larger_by) 
 
-
-  fun <- function(x) {
-    index = which(x == T)
-    if(index %>% length() == 0){ #if all false
-      index = 0
-    }
-    index
-  }
-  assignment_df$assignment = apply(assignment_df, 1, fun)
-  metadata_df = data.frame(assignment = assignment_df$assignment, row.names = rownames(assignment_df))
-  dataset = AddMetaData(object = dataset,metadata = metadata_df,col.name = "program.assignment")
+  dataset = AddMetaData(object = dataset,metadata = assignment_df,col.name = "program.assignment")
   return(dataset)
 }
+
+
+
