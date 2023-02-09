05c98e49d5fe09f22392aec546f80b0f9f3241b8
Avishai Wizel <Avishai987@gmail.com>
2023-02-09 12:54:37 +0200

    correct names for cell assignment

--- cnmf_function_Harmony.R
+++ cnmf_function_Harmony.R
@@ -9,7 +9,7 @@ program_assignment <- function(dataset,larger_by = 1,program_names) {
     n <- length(x)
     max = sort(x)[n] #get max value
     second_max = sort(x)[n-1] #get 2nd max value
-    if (max > larger_by*second_max){which.max(x)} else {"NA"} #if max is larger than 2nd max*larger_by, write max index. else, write "NA"
+    if (max > larger_by*second_max){program_names[which.max(x)]} else {"NA"} #if max is larger than 2nd max*larger_by, write max index. else, write "NA"
   }
   assignment_df["assigment"] = apply(X = scores_metadata,MARGIN = 1,FUN = find_max_program,larger_by = larger_by) 
   
